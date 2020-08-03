d91815f626035a8bbc54e765d7e1cf88ef8469d3
Jacob Wood <jjwood2893@gmail.com>
2020-08-03 13:50:59 -0400

    working on premium feature login finished

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="sidebar">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token === ''">Login</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+#app {
+  display: grid;
+  grid-template-columns: 20% auto;
+  grid-template-rows: auto;
+  height: 100vh;
+}
+
+.sidebar {
+  background-color: #236581;
+}
+</style>
