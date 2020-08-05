a68c205e7766c8e733e26962024d5a7dfec5fc14
Randy Proctor <randyproctor@gmail.com>
2020-08-05 10:04:28 -0400

    CSS and very lite html updates to App.vue and Home.vue.  Confirmed funtional

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,54 @@
 <template>
-  <div id="app">
+<div id="app">
+
     <div id="nav" class="sidebar">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link> <br>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token === ''">Login</router-link>
     </div>
-    <router-view />
-  </div>
+
+    <div class="content">
+      <router-view />
+    </div>
+
+</div>
 </template>
 
+<style>
+#app {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-areas: 
+  "navbar router-view";
+  grid-template-columns: 100px 1fr;
+}
+
+#nav {
+  grid-area: navbar;
+  background-color: #236581;
+}
+div.content {
+
+  grid-area: router-view;
+}
+
+a {
+  text-decoration: none;
+  color: seashell;
+  margin: 25px;
+  opacity: .5;
+
+}
+
+a.router-link-exact-active {
+  opacity: 1;
+}
+
+
+</style>
+
+<!--
 <style>
 #app {
   display: grid;
@@ -20,4 +60,10 @@
 .sidebar {
   background-color: #236581;
 }
+
+.sidebar > a {
+  justify-items: ;
+}
+
 </style>
+-->
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You don't have to be authenticated to see this.</p>
+
+    <div>
+      <img class="background" src="../assets/baw-comic.jpg" alt="Series of Comic Books">
+    </div>
+
+    <div class="header">
+    </div>
+
+    <div class="welcome-message">
+      <h1>Welcome to Three-Point!</h1>
+      <p wrap="hard">The ultimate site to log and share your comic collections! </p>
+    </div>
+
   </div>
 </template>
 
@@ -11,3 +22,45 @@ export default {
   name: 'home'
 };
 </script>
+
+<style>
+
+.home {
+  display: grid;
+  height: 100%;
+  width: 100%;
+  grid-template-columns: 1fr, 1fr, 1fr;
+  grid-template-rows: 1fr, 1fr, 1fr;
+  grid-template-areas: 
+  "header header header"
+  ". message ."
+  ". . .";
+}
+
+img {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  left: 0px;
+  top: 0px;
+  opacity: 0.3;
+  z-index: -1;
+}
+
+.welcome-message {
+  
+  background: rgb(255, 215, 36);
+  border: solid 2px black;
+  border-radius: 10px;
+  grid-area: message;
+  text-align: center;
+}
+
+.header {
+grid-area: header;
+
+}
+
+
+
+</style>
