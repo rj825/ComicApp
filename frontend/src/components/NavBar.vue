07c0af65fbce0586e2679a0dc7724227a8a7a798
Randy Proctor <randyproctor@gmail.com>
2020-08-11 11:43:03 -0400

    F.E. more navbar formatting.  Looks much better

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,29 +2,40 @@
   <div class="navbarroot">
 
         <b-navbar class="navbar"  toggleable="lg"  variant="info">
-            <router-link v-bind:to="{ name: 'home' }">
+            <router-link style="text-decoration: none;" class="logo" v-bind:to="{ name: 'home' }">
                 <img src="@/assets/threepoint.png" height="50px" width="53px"/>
+                THREE-POINT
             </router-link>
             
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item>
-                        <router-link v-bind:to="{path: `/collections/user/${username}`}" v-if="$store.state.token !=''">My Collection</router-link>
-                    </b-nav-item>
-                    <b-nav-item>
-                        <router-link v-bind:to="{ name: 'collections' }">Public Collections</router-link>
-                    </b-nav-item>
-                </b-navbar-nav>
+                
 
                 <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                        <b-nav-form>
+                <b-navbar-nav class="ml-auto" >
+                        <!-- <b-nav-form>
                             <b-form-input size="sm" class="mr-sm-2" placeholder="Search (not active)"></b-form-input>
                             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                        </b-nav-form>
+                        </b-nav-form> -->
+
+                        <b-navbar-nav>
+                            <b-nav-item>
+                                <router-link
+                                style="text-decoration: none;"
+                                v-bind:to="{path: `/collections/user/${username}`}" v-if="$store.state.token !=''">
+                                My Collection
+                                </router-link>
+                            </b-nav-item>
+                            <b-nav-item>
+                                <router-link
+                                style="text-decoration: none;"
+                                v-bind:to="{ name: 'collections' }">
+                                Public Collections
+                                </router-link>
+                            </b-nav-item>
+                        </b-navbar-nav>
 
                         <b-nav-item-dropdown right>
                              <!-- Using 'button-content' slot -->
@@ -64,6 +75,11 @@ export default {
 </script>
 
 <style>
+
+
+.logo {
+    font-size: x-large;
+}
 
 .navbarroot {
     font-family: "Bangers";
